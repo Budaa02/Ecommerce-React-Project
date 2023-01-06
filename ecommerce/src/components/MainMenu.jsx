c8f83149dc0ef2064fb3633d596eeb7dde03eebe
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-06 12:35:34 +0800

    day-37 nuuren heseg hiisen

--- ecommerce/src/components/MainMenu.jsx
+++ ecommerce/src/components/MainMenu.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import SubMenu from "./SubMenu"
 import { Nav, Navbar } from "react-bootstrap"
 import menus from "../data/menus"
+import { NavLink, NavDropdown } from "react-bootstrap";
 function MainMenu() {
 
     const subMenus = menus.map(subMenu => {
@@ -10,27 +11,39 @@ function MainMenu() {
             <SubMenu className="justify-content-end"
                 title={subMenu.title}
                 position={subMenu.position}
-                children={subMenu.children} 
+                children={subMenu.children}
             />
 
         )
     })
 
     return (
-        <div>
-            <div>Here is main menu</div>
-            <Navbar bg="light" expand="lg" className="">
-                <div className="browse_category">
-                    browse_categories
-                    
+        <div className="main-frame">
+
+            <Navbar bg="light" expand="lg" className="p-0">
+                <div className="container ">
+                    <div className="browse_category  d-flex me-0 frame-1 bg-warning">
+                        <p>Browse_categories </p>
+                        <NavDropdown>
+
+                        </NavDropdown>
+
+
+                    </div>
+                    <div className="d-flex me-5">
+                        {subMenus}
+                    </div>
+
+                    <div className="">
+                        30 Days Free Return
+                    </div>
                 </div>
-                {subMenus}
-                <div>
-                    30 Days Free Return
-                </div>
+
+
+
             </Navbar>
         </div>
-        
+
     )
 }
 console.log(Navbar);
