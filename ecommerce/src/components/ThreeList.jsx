566730b1e474e13553b0f015c257c5d2eed7cdcb
Morning <morning@mstars-lab2-06s-iMac.local>
2023-01-13 11:36:56 +0800

    day-42 ecommerce

--- ecommerce/src/components/ThreeList.jsx
+++ ecommerce/src/components/ThreeList.jsx
@@ -2,17 +2,8 @@
 export default function ThreeCards() {
     return (
         <div className="row container mainCards">
-            <div className="d-flex col-8 border rounded-4 m-1 p-3">
-                <img src="images/camera.png" />
-                <div>
-                    <p>Jbl bar 2.1 deep bass</p>
-                    <p>$11,70</p>
-                    <i></i>
-                    <button>Add to cart</button>
-                </div>
-            </div>
-            <div className="col rCard">
-                <div className="border rounded-4 m-1 d-flex p-2">
+            <div className="d-flex">
+                <div className="d-flex col-8 border rounded-4 m-1 p-3">
                     <img src="images/camera.png" />
                     <div>
                         <p>Jbl bar 2.1 deep bass</p>
@@ -20,19 +11,30 @@ export default function ThreeCards() {
                         <i></i>
                         <button>Add to cart</button>
                     </div>
-
                 </div>
-                <div className="border rounded-4 m-1 d-flex p-2 ">
-                    <img src="images/camera.png" />
-                    <div>
-                        <p>Jbl bar 2.1 deep bass</p>
-                        <p>$11,70</p>
-                        <i></i>
-                        <button>Add to cart</button>
-                    </div>
+                <div className="rCard col-4">
+                    <div className=" border rounded-4 m-1 d-flex p-2">
+                        <img src="images/camera.png" />
+                        <div>
+                            <p>Play game</p>
+                            <p>$11,70</p>
+                            <i></i>
+                        </div>
 
+                    </div>
+                    <div className=" border rounded-4 m-1 d-flex p-2 ">
+                        <img src="images/camera.png" />
+                        <div>
+                            <p>Play game</p>
+                            <p>$11,70</p>
+                            <i></i>
+
+                        </div>
+
+                    </div>
                 </div>
             </div>
+
         </div>
     )
 }
---
+++ ecommerce/src/components/footerLogo.jsx
@@ -0,0 +1,18 @@
+
+export default function LogoPng() {
+    return (
+        <div className="d-flex "  style={{height:"136px", alignItems:"center", justifyContent:"space-between"}}>
+            <img src="images/logo1.png" className=""/>
+            <img src="images/logo2.png" className=""/>
+            <img src="images/logo3.png" className=""/>
+            <img src="images/logo4.png" className=""/>
+            <img src="images/logo5.png" className=""/>
+        </div>
+    )
+}
+
+
+
+
+
+
